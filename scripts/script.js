3409fba525094af94ceac9da0186c0d34863fdbf
Halfonx <jermy.prigent@gmail.com>
2024-05-07 12:15:11 +0200

    Reset the JavaScript file

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-const buttons = document.querySelectorAll('.button');
-const screen = document.querySelector('#screen');
-
-buttons.forEach(function(button) {
-    button.addEventListener('click', function() {
-        
-    });
-});
