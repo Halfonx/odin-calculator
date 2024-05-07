c489834d69ac1c05c5791063192ad1bba31f3988
Halfonx <jermy.prigent@gmail.com>
2024-05-07 14:08:53 +0200

    Add class display in HTML and display these buttons in screen in JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
-const buttons = document.querySelectorAll('.button');
+const buttons = document.querySelectorAll('.display');
 const calculatorBottom = document.querySelector('#calculator_bottom');
+const screen = document.querySelector('#screen');
 
 buttons.forEach(function(button) {
     button.addEventListener('click', function() {
         console.log(button.value);
+        screen.innerText = button.value;
     })
 })
