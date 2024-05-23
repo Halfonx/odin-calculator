cfa5894dde634bfcf70e5dfd10732464721529b0
Halfonx <jermy.prigent@gmail.com>
2024-05-23 20:51:52 +0200

    Add data attributes in HTML and const selectors in JS

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const clearButton = document.querySelector('[data-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+const equalButton = document.querySelector('[data-equal]');
+const previousNumberText = document.querySelector('[data-previous-number]');
+const currentNumberText = document.querySelector('[data-current-number]');
