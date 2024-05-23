8e3098ec4348b79032871bb0bf2c0caff9829276
Halfonx <jermy.prigent@gmail.com>
2024-05-23 21:49:40 +0200

    Add class method and display a number in the calculator screen

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,33 @@ class Calculator {
     constructor(previousNumberText, currentNumberText) {
         this.previousNumberText = previousNumberText;
         this.currentNumberText = currentNumberText;
+        this.clear();
+    }
+
+    clear() {
+        this.currentNumber = '';
+        this.previousNumber = '';
+        this.operator = undefined;
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+        this.currentNumber = number;
+    }
+
+    chooseOperation(operator) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+        this.currentNumberText.innerText = this.currentNumber;
     }
 }
 
@@ -12,3 +39,12 @@ const deleteButton = document.querySelector('[data-delete]');
 const equalButton = document.querySelector('[data-equal]');
 const previousNumberText = document.querySelector('[data-previous-number]');
 const currentNumberText = document.querySelector('[data-current-number]');
+
+const calculator = new Calculator(previousNumberText, currentNumberText);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    })
+})
